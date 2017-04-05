8fb7268a9d9ef59ca8a9d8d5f689e6fa94dd2b52
WYF <WYF@wangyunfei>
2017-04-05 20:38:01 +0800

    maven demo

---
+++ src/main/java/my_maven_pro/maven_demo/App.java
@@ -0,0 +1,13 @@
+package my_maven_pro.maven_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello I'm app!" );
+    }
+}
---
+++ src/main/java/my_maven_pro/maven_demo/Server.java
@@ -0,0 +1,9 @@
+package my_maven_pro.maven_demo;
+
+public class Server {
+	
+	public static void main(String[] args) {
+		System.out.println("===>Hello I'm server");
+	}
+
+}
