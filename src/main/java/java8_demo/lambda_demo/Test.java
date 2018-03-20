1a36733dd4a13c30d3552b6c41f648c48ce9fd2d
wang.yunfei <wb-wyf310649@cainiao.com>
2018-03-20 19:10:01 +0800

    test

--- src/main/java/java8_demo/lambda_demo/Test.java
+++ src/main/java/java8_demo/lambda_demo/Test.java
@@ -51,6 +51,7 @@ public class Test {
         //test revert
         //test revert123
         //test reset
+        //test
     }
 
     public static Map<Long,UserInfo> testCollectToMap(List<UserInfo> list){
