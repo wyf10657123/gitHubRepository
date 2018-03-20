dcff265b05739aad3f64293f15d6151e9c8f8182
wang.yunfei <wb-wyf310649@cainiao.com>
2018-03-20 16:40:18 +0800

    test revert123

--- src/main/java/java8_demo/lambda_demo/Test.java
+++ src/main/java/java8_demo/lambda_demo/Test.java
@@ -49,6 +49,7 @@ public class Test {
         //eagerEvaluator(evaluate(1),evaluate(2));
         lazyEvaluator(()->evaluate(1),()->evaluate(2));//懒操作
         //test revert
+        //test revert123
     }
 
     public static Map<Long,UserInfo> testCollectToMap(List<UserInfo> list){
